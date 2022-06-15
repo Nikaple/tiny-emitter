b04c386c9b1c03335ed86a8867776bdfce7bc307
nikaple <nikaple1@gmail.com>
2022-06-15 18:08:10 +0800

    Update index.d.ts

--- index.d.ts
+++ index.d.ts
@@ -1,10 +1,10 @@
 type Arguments<T> = [T] extends [(...args: infer U) => any] ? U : [T] extends [void] ? [] : [T];
 
-export declare class TinyEmitter<T extends any = any> {
+export declare class TinyEmitter<T extends any = any, R extends any = T> {
   on<E extends keyof T>(event: E, callback: T[E], ctx?: any): this;
   once<E extends keyof T>(event: E, callback: T[E], ctx?: any): this;
-  emit<E extends keyof T>(event: E, ...args: Arguments<T[E]>): this;
   off<E extends keyof T>(event: E, callback?: T[E]): this;
+  emit<E extends keyof R>(event: E, ...args: Arguments<R[E]>): this;
 }
 
 interface TinyEmitterStatic {
